d4380d83a3d847780a1d72aad5af636484f0e7b8
Rouge <14047458+rougetimelord@users.noreply.github.com>
2023-09-24 19:57:36 -0700

    Make popup better
    Use background image and psuedo elements so the event target isn't fucky.

--- popup/index.js
+++ popup/index.js
@@ -1,28 +1,17 @@
 //Detect chrome
-let extStore = !!chrome ? chrome.storage : browser.storage;
-//Basic bitch event listener, send of whatever user picked
-let react = elem => {
-    document.getElementsByClassName("sel").item(0).classList.remove("sel");
-    elem.classList.add("sel");
-    extStore.sync.set({
-        theme: elem.id.replace("opt","")
-    });
-}
+let runtimeStorage = !!chrome ? chrome.storage : browser.storage;
 //Add event listener
 document.body.addEventListener("click", event => {
-    if (event.target.classList.contains("opt")){
-        react(event.target);
-    }
-    if (event.target.parentNode.classList.contains("opt")){
-        react(event.target.parentNode)
-    }
+    document.getElementsByClassName("sel").item(0).classList.remove("sel");
+    elem.classList.add("sel");
+    runtimeStorage.sync.set({
+        theme: elem.id.replace("opt","")
+    });
 });
 //Load saved theme or default
 document.addEventListener("DOMContentLoaded", () => {
-    extStore.sync.get({theme: 1}).then(res => {
+    runtimeStorage.sync.get({theme: 1}).then(res => {
         document.getElementsByClassName("sel").item(0).classList.remove("sel");
         document.getElementById(`opt${res.theme}`).classList.add("sel");
-    }, err => {
-        console.log(`Error: ${err}`);
-    })
+    });
 })
