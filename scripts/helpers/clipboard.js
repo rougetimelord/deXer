e8f1f3f949ef3ca0247a6d9c770249d486142598
Rouge <14047458+rougetimelord@users.noreply.github.com>
2023-09-25 13:18:29 -0700

    Make intercept async

--- scripts/helpers/clipboard.js
+++ scripts/helpers/clipboard.js
@@ -1,4 +1,4 @@
-export const intercept = () => {window.addEventListener("copy", async event => {
+export const intercept = async () => {window.addEventListener("copy", async event => {
     event.preventDefault();
     let val = event.target.innerText.replace("x.com","twitter.com");
     event.clipboardData.setData("text/plain", val);
