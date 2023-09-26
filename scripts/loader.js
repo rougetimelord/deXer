69a9268a2f5a67c725cf7cbc8011d934eac91fed
Rouge <14047458+rougetimelord@users.noreply.github.com>
2023-09-25 23:20:53 -0700

    Remove overwrite from loader

--- scripts/loader.js
+++ scripts/loader.js
@@ -1,12 +1,4 @@
 (async () => {
-    const orig = history.pushState;
-    history.pushState = async () => {
-        var pushStateReturn = orig.apply(this, arguments);
-        var event = new Event("pushstate");
-        event.state = arguments[0];
-        window.dispatchEvent(event);
-        return pushStateReturn;
-    };
     const inject = await import(
         (!!chrome ? chrome : browser).runtime
         .getURL("/scripts/inject.js"));
