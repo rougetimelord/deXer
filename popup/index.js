e01c70cbb1a16166bf3f10d609aacaa6900b7302
Rouge <14047458+rougetimelord@users.noreply.github.com>
2023-10-11 00:22:27 -0700

    Add localization support

--- popup/index.js
+++ popup/index.js
@@ -1,16 +1,17 @@
 //Detect chrome
-let runtimeStorage = !!chrome ? chrome.storage : browser.storage;
+let runtime = !!chrome ? chrome : browser;
 //Add event listener
 document.body.addEventListener("click", event => {
     document.getElementsByClassName("sel").item(0).classList.remove("sel");
     event.target.classList.add("sel");
-    runtimeStorage.sync.set({
+    runtime.storage.sync.set({
         theme: event.target.id.replace("opt","")
     });
 });
 //Load saved theme or default
 document.addEventListener("DOMContentLoaded", () => {
-    runtimeStorage.sync.get({theme: 1}).then(res => {
+    document.getElementById("desc").innerText = runtime.i18n.getMessage(themeDescription)
+    runtime.storage.sync.get({theme: 1}).then(res => {
         document.getElementsByClassName("sel").item(0).classList.remove("sel");
         document.getElementById(`opt${res.theme}`).classList.add("sel");
     });
