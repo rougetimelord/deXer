5faa13c9ed57dc9f2e9d8244c49b7c6fe248b68f
Rouge <14047458+rougetimelord@users.noreply.github.com>
2023-11-01 20:17:01 -0700

    Add support in content scripts

--- src/utils/clipboard.js
+++ src/utils/clipboard.js
@@ -1,18 +1,29 @@
+import { storageGetter, storageListener } from "./runtime.js";
+
+let fx = (await storageGetter({fx: false}))["fx"];
+storageListener(value => fx = value, "fx")
+
+/**
+ * Handles copy events
+ * @param {ClipboardEvent} event 
+ */
+const copyHandler = async (event) => {
+  if (event.target.style.opacity == "0") {
+    event.preventDefault();
+    let val = event.target.innerText
+      .replace("x.com", (fx) ? "fxtwiiter.com" : "twitter.com")
+      .split(/\?\w+=/)[0];
+    event.clipboardData.setData("text/plain", val);
+  };
+};
+
 /**
  * Listens for copies and interrupts them
  */
 export default async () => {
   window.addEventListener(
     "copy",
-    async (event) => {
-      if (event.target.style.opacity == "0") {
-        event.preventDefault();
-        let val = event.target.innerText
-          .replace("x.com", "twitter.com")
-          .split(/\?\w+=/)[0];
-        event.clipboardData.setData("text/plain", val);
-      }
-    },
+    copyHandler,
     { capture: false },
   );
 };
