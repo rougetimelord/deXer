b0023af37308dd78df58d1dfdae4529bff60977a
Rouge <14047458+rougetimelord@users.noreply.github.com>
2023-11-07 00:12:05 -0800

    Fix unsafe eval crash

--- src/utils/index.js
+++ src/utils/index.js
@@ -17,7 +17,7 @@ export const notificationTweet = i18nGetter("notificationTweet");
  * @param {number} ms
  * @returns A promise that resolves after a `ms` wait
  */
-export const delay = async (ms) => new Promise((res) => setTimeout(res(0), ms));
+export const delay = async (ms) => new Promise((res) => {setTimeout(res, ms)});
 
 /**
  * @type {Object} logos
