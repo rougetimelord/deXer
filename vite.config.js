fd7547c8c413763e73a038331519abf8274e531a
Rouge <14047458+rougetimelord@users.noreply.github.com>
2024-10-21 14:37:27 -0700

    [fix] bump vite-plugin to fixed version for Chrome 130+

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import {defineConfig} from 'vite';
 import {crx} from '@crxjs/vite-plugin';
-import manifest from './src/manifest';
+import manifest from './src/manifest.js';
 
 export default defineConfig(() => {
     return {
